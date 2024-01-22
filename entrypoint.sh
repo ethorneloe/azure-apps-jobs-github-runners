73fb8a30bbc707345e92e573dc2ac7759c7c4b5e
ethorneloe <eric.thorneloe@daf.qld.gov.au>
2024-01-22 20:53:56 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,47 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
-# Retrieve a short lived runner registration token using the PAT
-REGISTRATION_TOKEN="$(curl -X POST -fsSL \
+set -o pipefail
+
+now=$(date +%s)
+iat=$((${now} - 60)) # Issued 60 seconds in the past
+exp=$((${now} + 600)) # Expires 10 minutes in the future
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+    "typ":"JWT",
+    "alg":"RS256"
+}'
+header=$(echo -n "${header_json}" | b64enc)
+
+payload_json='{
+    "iat":'"${iat}"',
+    "exp":'"${exp}"',
+    "iss":'"${APP_ID}"'
+}'
+payload=$(echo -n "${payload_json}" | b64enc)
+
+header_payload="${header}.${payload}"
+signature=$(openssl dgst -sha256 -sign <(echo -n "${PEM}") <(echo -n "${header_payload}") | b64enc)
+
+jwt="${header_payload}.${signature}"
+printf '%s\n' "JWT: $jwt"
+
+# Get an access token from the API using the installation ID of the GitHub app.
+access_token=$(curl -X POST -fsSL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $jwt" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "$ACCESS_TOKEN_API_URL" \
+  | jq -r '.token')
+
+# Retrieve a short lived runner registration token using the access token.
+registration_token=$(curl -X POST -fsSL \
   -H 'Accept: application/vnd.github.v3+json' \
-  -H "Authorization: Bearer $GITHUB_PAT" \
+  -H "Authorization: Bearer $access_token" \
   -H 'X-GitHub-Api-Version: 2022-11-28' \
   "$REGISTRATION_TOKEN_API_URL" \
-  | jq -r '.token')"
+  | jq -r '.token')
 
-./config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
+# Register the ephemeral runner
+./config.sh --url $REPO_URL --token $registration_token --unattended --ephemeral && ./run.sh
